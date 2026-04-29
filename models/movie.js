f5157516fdd9f1b7823b85bb9585e693ebadf0b2
Dilshan454 <dilshanchathuranga1634@gmail.com>
2026-04-29 19:36:57 +0530

    create models

---
+++ models/movie.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const movieSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+  director: String,
+  year: Number,
+  genre: String,
+  rating: { type: Number, min: 0, max: 10 }
+}, { timestamps: true });
+
+const Movie=mongoose.model("Movie",movieSchema);
+export default Movie;
