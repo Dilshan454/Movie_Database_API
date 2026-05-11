01007a48b9977b68fe48b8747d66b546bc3f536a
Dilshan454 <dilshanchathuranga1634@gmail.com>
2026-05-11 16:15:30 +0530

    controllers and models

---
+++ routes/movieRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+import {
+    createMovie,
+    getMovies,
+    updateMovie,
+    deleteMovie
+} from "../controllers/movieController.js";
+
+const router = express.Router();
+
+router.post("/", createMovie);
+router.get("/", getMovies);
+router.put("/:id", updateMovie);
+router.delete("/:id", deleteMovie);
+
+export default router;
