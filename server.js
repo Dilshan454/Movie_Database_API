b0028fa3fd099df0334dd4584ec86f2e66d72b1e
Dilshan454 <dilshanchathuranga1634@gmail.com>
2026-05-15 13:57:53 +0530

    fix errors

--- server.js
+++ server.js
@@ -11,13 +11,17 @@ import reviewsRoutes from "./routes/reviewsRoutes.js";
 const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-const MONGOURL = process.env.MONGO_URL;
-
 
 app.use(cors());
 app.use(express.json());
 
+
+const PORT = process.env.PORT || 5000;
+const MONGOURL = process.env.MONGO_URL;
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 mongoose
 .connect(MONGOURL)
